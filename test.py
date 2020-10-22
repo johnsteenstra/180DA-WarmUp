795343676df1815f62df1932dc62c7a72b3891c4
johnsteenstra <johnsteenstra@gmail.com>
2020-10-22 20:42:12 +0100

    Typo fix

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
 	x = "ECE_180_DA_DB"
 	if x == "EE_180_DA_DB":
-		print("You are living in 22017")
+		print("You are living in 2017")
 	else:
 		#this is a comment
 		x = x + " - Best class ever"
